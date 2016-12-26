35a5942fdaf0cf0badf27ab63654b4fb79cd10b6
susuyan <susuyan@163.com>
2016-12-26 10:54:02 +0800

    initial

---
+++ Bother/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Bother
+//
+//  Created by 余胜民 on 16/9/27.
+//  Copyright © 2016年 susuyan. All rights reserved.
+//
+
+import UIKit
+import CallKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if #available(iOS 10.0, *) {
+
+
+            
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
